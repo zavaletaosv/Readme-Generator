6ad56db5509bffb46495e8472d098156a8ad3980
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-09-07 08:00:15 -0500

    index.js updated: packages imported

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// TODO: Include packages needed for this application
+// Imported Packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
