de17d744bcad5de4e0d266fa5b639dc4b141eb7a
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-05 08:31:33 -0500

    utils/generateMardown.js updated: function to render GNU license section added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -81,6 +81,23 @@ function renderLicenseSection(license, name, year) {
                     limitations under the License.`
                 break;
 
+                case 'GNU GPLv3':
+                    return `Copyright (C) ${year}  ${name}
+        
+                        This program is free software: you can redistribute it and/or modify
+                        it under the terms of the GNU General Public License as published by
+                        the Free Software Foundation, either version 3 of the License, or
+                        (at your option) any later version.
+                    
+                        This program is distributed in the hope that it will be useful,
+                        but WITHOUT ANY WARRANTY; without even the implied warranty of
+                        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                        GNU General Public License for more details.
+                    
+                        You should have received a copy of the GNU General Public License
+                        along with this program.  If not, see <https://www.gnu.org/licenses/>.`
+                    break;
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
