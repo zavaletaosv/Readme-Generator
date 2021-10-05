a431666a30800c228df78afcf2ca8ffe327640f6
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-05 08:28:44 -0500

    utils/generateMardown.js updated: function to render license link created

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,10 +24,31 @@ function renderLicenseBadge(license) {
     }
 }
 
+// Create a Function That Returns the License Link
+// If there is No License, Return An Empty String
+function renderLicenseLink(license) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    switch (license) {
+        case 'MIT':
+            return '(https://opensource.org/licenses/MIT)'
+            break;
+
+        case 'Apache License 2.0':
+            return '(https://opensource.org/licenses/Apache-2.0)'
+            break;
+
+        case 'GNU GPLv3':
+            return '(https://www.gnu.org/licenses/gpl-3.0)'
+            break;
+
+        case 'Mozilla Public License 2.0':
+            return'(https://opensource.org/licenses/MPL-2.0)'
+            break;
+
+        default: ''
+            break;
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
