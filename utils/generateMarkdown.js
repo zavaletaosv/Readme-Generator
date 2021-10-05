c7f0aa3687c2144dbd3f9b9a2c3cb637d0aa065b
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-05 08:27:55 -0500

    utils/generateMardown.js updated: function to render license badge created

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,29 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Create a Function That Returns a License Badge Based on Which License Is Passed In
+// If There Is No License, Return An Empty String
+function renderLicenseBadge(license) {
+
+    switch (license) {
+        case 'MIT':
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            break;
+
+        case 'Apache License 2.0':
+            return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            break;
+
+        case 'GNU GPLv3':
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+            break;
+
+        case 'Mozilla Public License 2.0':
+            return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+            break;
+
+        default: ''
+            break;
+    }
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
